b17ecaa0b3b5f9fb14f7b9217f9806ac1194ab16
daitokojima <daito.kojima@salto.link>
2025-01-25 14:20:44 +0900

    changed directory

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://your-fastapi-app.up.railway.app',
+    baseURL: 'financeapp.railway.internal',
 });
 
 export default api;
